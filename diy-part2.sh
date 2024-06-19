0afcc4a332a5ae8f930e370bc8ea04d6907d9a30
wildOldCoder <80471617+wildOldCoder@users.noreply.github.com>
2024-06-19 08:56:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,12 +26,12 @@ function git_sparse_clone() {
 
 # 添加额外插件
 git clone -b js --single-branch https://github.com/gngpp/luci-theme-design package/luci-theme-design
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-aliddns
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-pushbot
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-jellyfin luci-lib-taskd luci-lib-xterm taskd
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-linkease linkease ffmpeg-remux
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-passwall2
+# git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-pushbot
+# git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-jellyfin luci-lib-taskd luci-lib-xterm taskd
+# git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-linkease linkease ffmpeg-remux
 
 # 加入OpenClash核心
 chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
@@ -41,20 +41,23 @@ echo "
 # 主题
 CONFIG_PACKAGE_luci-theme-design=y
 
+# passwall2
+CONFIG_PACKAGE_luci-app-passwall2=y
+
 # mosdns
-CONFIG_PACKAGE_luci-app-mosdns=y
+# CONFIG_PACKAGE_luci-app-mosdns=y
 
 # pushbot
-CONFIG_PACKAGE_luci-app-pushbot=y
+# CONFIG_PACKAGE_luci-app-pushbot=y
 
 # 阿里DDNS
-CONFIG_PACKAGE_luci-app-aliddns=y
+# CONFIG_PACKAGE_luci-app-aliddns=y
 
 # Jellyfin
 #CONFIG_PACKAGE_luci-app-jellyfin=y
 
 # 易有云
-CONFIG_PACKAGE_luci-app-linkease=y
+# CONFIG_PACKAGE_luci-app-linkease=y
 
 " >> .config
 
